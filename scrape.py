6b7a74fea9fd387de57321314dde06b36fd25e92
Henry Rossiter <henry.rossiter@utexas.edu>
2020-03-25 11:51:07 -0500

    generate histograms

--- scrape.py
+++ scrape.py
@@ -3,23 +3,26 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 import re
 
-driver = webdriver.Chrome('./chromedriver')
-driver.get("https://play.esea.net/index.php?s=league&d=bracket&id=7572")
-	
-driver.implicitly_wait(10)
-elements = driver.find_elements_by_class_name('match-games')
 
-score_tuples = []
-for element in elements:
-    if element.text and element.text != '':
-        text = element.text
-        regex = r"\D*(\d{1,2})\s*-\s*(\d{1,2})\D*"
-        scores = re.findall(regex, text)
-        for score in scores:
-            score1 = int(score[0])
-            score2 = int(score[1])
-            score_tuples.append((score1, score2))
-            
+def scrapeBracket(driver, bracketId):
+    driver.get('https://play.esea.net/index.php?s=league&d=bracket&id={}'.format(bracketId))
+        
+    driver.implicitly_wait(10)
+    elements = driver.find_elements_by_class_name('match-games')
+    score_tuples = []
+    for element in elements:
+        if element.text and element.text != '':
+            text = element.text
+            regex = r"\D*(\d{1,2})\s*-\s*(\d{1,2})\D*"
+            scores = re.findall(regex, text)
+            for score in scores:
+                score1 = int(score[0])
+                score2 = int(score[1])
+                score_tuples.append((score1, score2))
+    return score_tuples
+                
+
+
 
 
 
